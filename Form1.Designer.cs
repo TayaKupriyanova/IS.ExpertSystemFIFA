c439ab1a4b2fda54b2de140ba7c82f4708f322a8
TayaKupriyanova <74012985+TayaKupriyanova@users.noreply.github.com>
2021-11-22 14:20:09 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ExpertSystem
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btime = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ChBLower = new System.Windows.Forms.CheckBox();
+            this.ChBHigher = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CBAt = new System.Windows.Forms.ComboBox();
+            this.CBDef = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Bbudg = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.GetRes = new System.Windows.Forms.Button();
+            this.TBRes = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Btime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bbudg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сколько часов в неделю вы играете в FIFA?  (1-18)";
+            // 
+            // Btime
+            // 
+            this.Btime.Location = new System.Drawing.Point(94, 118);
+            this.Btime.Maximum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.Btime.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.Btime.Name = "Btime";
+            this.Btime.Size = new System.Drawing.Size(58, 20);
+            this.Btime.TabIndex = 1;
+            this.Btime.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Оцените уровень вашего мастерства:";
+            // 
+            // ChBLower
+            // 
+            this.ChBLower.AutoSize = true;
+            this.ChBLower.Location = new System.Drawing.Point(15, 35);
+            this.ChBLower.Name = "ChBLower";
+            this.ChBLower.Size = new System.Drawing.Size(104, 17);
+            this.ChBLower.TabIndex = 3;
+            this.ChBLower.Text = "Ниже среднего";
+            this.ChBLower.UseVisualStyleBackColor = true;
+            // 
+            // ChBHigher
+            // 
+            this.ChBHigher.AutoSize = true;
+            this.ChBHigher.Location = new System.Drawing.Point(132, 35);
+            this.ChBHigher.Name = "ChBHigher";
+            this.ChBHigher.Size = new System.Drawing.Size(105, 17);
+            this.ChBHigher.TabIndex = 4;
+            this.ChBHigher.Text = "Выше среднего";
+            this.ChBHigher.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(222, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Выберите предпочтительный стиль атаки:";
+            // 
+            // CBAt
+            // 
+            this.CBAt.FormattingEnabled = true;
+            this.CBAt.Items.AddRange(new object[] {
+            "Быстрые атаки",
+            "Владение мячом",
+            "Контрвыпады"});
+            this.CBAt.Location = new System.Drawing.Point(315, 35);
+            this.CBAt.Name = "CBAt";
+            this.CBAt.Size = new System.Drawing.Size(209, 21);
+            this.CBAt.TabIndex = 7;
+            this.CBAt.Text = "Стиль атаки";
+            // 
+            // CBDef
+            // 
+            this.CBDef.FormattingEnabled = true;
+            this.CBDef.Items.AddRange(new object[] {
+            "Агрессивный прессинг",
+            "Умеренный прессинг",
+            "Оборона на своей половине"});
+            this.CBDef.Location = new System.Drawing.Point(315, 107);
+            this.CBDef.Name = "CBDef";
+            this.CBDef.Size = new System.Drawing.Size(209, 21);
+            this.CBDef.TabIndex = 9;
+            this.CBDef.Text = "Стиль защиты";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выберите предпочтительный стиль защиты:";
+            // 
+            // Bbudg
+            // 
+            this.Bbudg.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.Bbudg.Location = new System.Drawing.Point(715, 73);
+            this.Bbudg.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.Bbudg.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.Bbudg.Name = "Bbudg";
+            this.Bbudg.Size = new System.Drawing.Size(79, 20);
+            this.Bbudg.TabIndex = 11;
+            this.Bbudg.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(543, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(543, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Введите доступный бюджет на формирование ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(543, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "команды в тысячах (100-1000)";
+            // 
+            // GetRes
+            // 
+            this.GetRes.Location = new System.Drawing.Point(623, 147);
+            this.GetRes.Name = "GetRes";
+            this.GetRes.Size = new System.Drawing.Size(135, 36);
+            this.GetRes.TabIndex = 15;
+            this.GetRes.Text = "Получить результат!";
+            this.GetRes.UseVisualStyleBackColor = true;
+            this.GetRes.Click += new System.EventHandler(this.GetRes_Click);
+            // 
+            // TBRes
+            // 
+            this.TBRes.Location = new System.Drawing.Point(44, 200);
+            this.TBRes.Multiline = true;
+            this.TBRes.Name = "TBRes";
+            this.TBRes.Size = new System.Drawing.Size(714, 216);
+            this.TBRes.TabIndex = 16;
+            this.TBRes.Text = "Здесь может быть ваш результат";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 442);
+            this.Controls.Add(this.TBRes);
+            this.Controls.Add(this.GetRes);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Bbudg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CBDef);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CBAt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ChBHigher);
+            this.Controls.Add(this.ChBLower);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Btime);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "Экспертная система FIFA";
+            ((System.ComponentModel.ISupportInitialize)(this.Btime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bbudg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown Btime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox ChBLower;
+        private System.Windows.Forms.CheckBox ChBHigher;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CBAt;
+        private System.Windows.Forms.ComboBox CBDef;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown Bbudg;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button GetRes;
+        private System.Windows.Forms.TextBox TBRes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExpertSystem
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string STYLEGA, STYLEGD;
+        string errorChecked = "Ошибка в указании мастерства. Выберите один вариант.";
+        string errorStyle = "Ошибка в указании стиля игры. Необходимо выбрать один стиль атаки и один стиль защиты";
+        SchemeRule[] schemeRules;
+        TacticRule[] tacticRules;
+        TasksRule[] tasksRules;
+        SquadRule[] squadRules;
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void GetRes_Click(object sender, EventArgs e)
+        {
+            // проверяем правила на наличие ошибок
+            CheckData();
+
+            // считываем введенную пользователем информацию в специальный класс
+            ActualData actualData = new ActualData((int)Bbudg.Value, CBAt.SelectedItem.ToString(), CBDef.SelectedItem.ToString(), (int)Btime.Value, ChBLower.Checked);
+            TBRes.Clear();
+
+            // заполняем базу знаний правилами
+            LoadRules();        
+            
+            // выбираем из всей базы правила, подходящие нам
+            ActualRules actualRules = new ActualRules();
+            actualRules.getActualRules(schemeRules, tacticRules, tasksRules, squadRules, actualData);
+
+            // берем из него все следствия и копируем в IntermediateGoals
+            IntermediateGoals IG = new IntermediateGoals(actualRules);
+            
+            // выводим его в TBRrs
+            TBRes.Text = "Схема игры: " + IG.scheme + Environment.NewLine + Environment.NewLine + "Тактика игры: " + IG.tactic + Environment.NewLine + Environment.NewLine + "Задачи игрокам: " + IG.tasks + Environment.NewLine + Environment.NewLine + " Состав команды: " + IG.squad;
+
+
+
+        }
+
+        void CheckData()
+        {
+            // здесь должна быть проверка введенных результатов
+            string error = " ";
+            if ((ChBLower.Checked & ChBHigher.Checked) | (!ChBLower.Checked & !ChBHigher.Checked)) error += errorChecked;
+            if ((STYLEGA == null) | (STYLEGD == null)) error += errorStyle;
+
+             try
+             {
+                 if ((ChBLower.Checked & ChBHigher.Checked) | (!ChBLower.Checked & !ChBHigher.Checked)) error += errorChecked;
+                 STYLEGA = CBAt.SelectedItem.ToString();
+                 STYLEGD = CBDef.SelectedItem.ToString();
+
+             }
+
+             catch (System.NullReferenceException)
+             {
+                 error += errorStyle;
+                System.Windows.Forms.Application.Exit();
+            }
+
+        }
+
+        void LoadRules()
+        {
+            // подключить и привязать эксель
+            Microsoft.Office.Interop.Excel.Application DB = new Microsoft.Office.Interop.Excel.Application();
+
+            Workbook exel = DB.Workbooks.Open(@"D:\Common (G)\Учеба)0)(((000\7 семестр (текущий)\Интеллектуальные системы\лабы\лаба1\Database.xlsx");
+            _Worksheet schemeSheet = exel.Sheets[1];
+            _Worksheet tacticSheet = exel.Sheets[2];
+            _Worksheet tasksSheet = exel.Sheets[3];
+            _Worksheet squadSheet = exel.Sheets[4];
+
+            Range schemeRange = schemeSheet.UsedRange;
+            Range tacticRange = tacticSheet.UsedRange;
+            Range tasksRange = tasksSheet.UsedRange;
+            Range squadRange = squadSheet.UsedRange;
+
+            // перенести информацию из базы знаний в объекты
+            int schemeN = 36;
+            schemeRules = new SchemeRule[schemeN];
+            string scheme, stylea, styled; int mast, timeH, timeL; 
+            for ( int i =0; i< schemeN; i++)
+            {
+                scheme = schemeRange.Cells[i + 2, 1].Value2.ToString();
+                stylea = schemeRange.Cells[i + 2, 5].Value2.ToString();
+                styled = schemeRange.Cells[i + 2, 6].Value2.ToString();
+                timeH = Convert.ToInt32(schemeRange.Cells[i + 2, 3].Value2.ToString());
+                timeL = Convert.ToInt32(schemeRange.Cells[i + 2, 4].Value2.ToString());
+                mast = Convert.ToInt32(schemeRange.Cells[i + 2, 2].Value2.ToString());
+
+                schemeRules[i] = new SchemeRule(mast, timeH, timeL, stylea, styled, scheme);
+            }
+            
+            int tacticN = 9;
+            tacticRules = new TacticRule[tacticN];
+            for (int i = 0; i < tacticN; i++)
+            {
+                tacticRules[i] = new TacticRule(tacticRange.Cells[i + 2, 2].Value2.ToString(), tacticRange.Cells[i + 2, 3].Value2.ToString(), tacticRange.Cells[i + 2, 1].Value2.ToString());
+            }
+
+            int tasksN = 9;
+            tasksRules = new TasksRule[tasksN];
+            for (int i = 0; i < tasksN; i++)
+            {
+                tasksRules[i] = new TasksRule(tasksRange.Cells[i + 2, 2].Value2.ToString(), tasksRange.Cells[i + 2, 3].Value2.ToString(), tasksRange.Cells[i + 2, 1].Value2.ToString());
+            }
+
+            int squadN = 27;
+            squadRules = new SquadRule[squadN];
+            int budgH, budgL;
+            for (int i = 0; i < squadN; i++)
+            {
+                budgH = Convert.ToInt32(squadRange.Cells[i + 2, 2].Value2.ToString());
+                budgL = Convert.ToInt32(squadRange.Cells[i + 2, 3].Value2.ToString());
+
+                squadRules[i] = new SquadRule(budgH, budgL, squadRange.Cells[i + 2, 4].Value2.ToString(), squadRange.Cells[i + 2, 5].Value2.ToString(), squadRange.Cells[i + 2, 1].Value2.ToString());
+            }
+
+            // завершаем работу экселя
+            DB.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(DB);
+
+        }
+
+    }
+
+    public class ActualData {
+        public int budget;
+        public string styleA;
+        public string styleD;
+        public int timeg;
+        public int mast;
+
+        public ActualData(int BUDG, string STYLEA, string STYLED, int TIME, bool LOW)
+        {
+            budget = BUDG;
+            styleA = STYLEA;
+            styleD = STYLED;
+            timeg = TIME;
+            if (LOW) mast = 0;
+            else mast = 1;
+        }
+    }
+    public class ActualRules {
+        public SchemeRule schemeRule;
+        public TacticRule tacticRule;
+        public TasksRule tasksRule;
+        public SquadRule squadRule;
+
+        public ActualRules()
+        {
+            schemeRule = null;
+            tacticRule = null;
+            tasksRule = null;
+            squadRule = null;
+        }
+
+        public void getActualRules(SchemeRule[] schR, TacticRule[] tacR, TasksRule[] taskR, SquadRule[] squadR, ActualData AD)
+        {
+
+            // проходим по массивам объектов с правилами и выбираем подходящие, сравнивания с объектом AD
+            for (int i =0; i< schR.Length; i++)
+            {
+                if (schR[i].isTrue(AD.mast, AD.timeg, AD.styleA, AD.styleD))
+                {
+                    schemeRule = schR[i];
+                    break;
+                }
+            }
+
+            for (int i =0; i<tacR.Length; i++)
+            {
+                if(tacR[i].isTrue(AD.styleA, AD.styleD))
+                {
+                    tacticRule = tacR[i];
+                    break;
+                }
+            }
+
+            for (int i =0; i< taskR.Length; i++)
+            {
+                if(taskR[i].isTrue(AD.styleA, AD.styleD))
+                {
+                    tasksRule = taskR[i];
+                    break;
+                }
+            }
+
+            for( int i =0; i< squadR.Length; i++)
+            {
+                if(squadR[i].isTrue(AD.budget, AD.styleA, AD.styleD))
+                {
+                    squadRule = squadR[i];
+                    break;
+                }
+            }
+        }
+    }
+    public class IntermediateGoals {
+        public string scheme;
+        public string tactic;
+        public string tasks;
+        public string squad;
+
+        public IntermediateGoals(ActualRules actualRules) {
+            scheme = actualRules.schemeRule.schemeG;
+            tactic = actualRules.tacticRule.tactic;
+            tasks = actualRules.tasksRule.tasks;
+            squad = actualRules.squadRule.squad;
+        }
+
+    }
+    public class GOALVAR {
+        public string result;
+
+        public GOALVAR(IntermediateGoals IG)
+        {
+        }
+
+    }
+    public class SchemeRule {
+        public int mast;
+        public int timeH;
+        public int timeL;
+        public string styleA;
+        public string styleD;
+        public string schemeG;
+
+        public SchemeRule(int M, int TH, int TL, string SA, string SD, string SC) {
+            mast = M;
+            timeH = TH;
+            timeL = TL;
+            styleA = SA;
+            styleD = SD;
+            schemeG = SC;
+        }
+        public bool isTrue(int M, int T, string SA, string SD)
+        {
+            if ((mast == M) & (T <= timeH) &(T>=timeL) & (styleA == SA) & (styleD == SD))
+                return true;
+            else return false;
+        }
+
+    }
+    public class TacticRule {
+        public string styleA;
+        public string styleD;
+        public string tactic;
+
+        public TacticRule(string SA, string SD, string T) {
+            styleA = SA;
+            styleD = SD;
+            tactic = T;
+        }
+        public bool isTrue(string SA, string SD)
+        {
+            if ((styleA == SA) & (styleD == SD))
+                return true;
+            else return false;
+        }
+    }
+    public class TasksRule {
+        public string styleA;
+        public string styleD;
+        public string tasks;
+
+        public TasksRule(string SA, string SD, string T) {
+            styleA = SA;
+            styleD = SD;
+            tasks = T;
+        }
+        public bool isTrue(string SA, string SD)
+        {
+            if ((styleA == SA) & (styleD == SD))
+                return true;
+            else return false;
+        }
+    }
+    public class SquadRule {
+        public int budgetH;
+        public int budgetL;
+        public string styleA;
+        public string styleD;
+        public string squad;
+
+        public SquadRule(int BH, int BL, string SA, string SD, string T) {
+            budgetH = BH;
+            budgetL = BL;
+            styleA = SA;
+            styleD = SD;
+            squad = T;
+        }
+        public bool isTrue(int B, string SA, string SD)
+        {
+            if ((B <= budgetH) & (B >= budgetL) & (styleA == SA) & (styleD == SD))
+                return true;
+            else return false;
+        }
+    }
+
+}
